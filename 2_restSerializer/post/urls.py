bc53804648588f963b3870a39a9c12b8b2b64a96
CaesiumY <mn0658@naver.com>
2019-11-16 14:19:48 +0900

    feat: Add rest server

--- 2_restSerializer/post/urls.py
+++ 2_restSerializer/post/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
-import post.urls
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register('post', views.postViewset)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('post.urls')),
+    path('', include(router.urls)),
 ]
