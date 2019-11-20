c29eb59bb17aa973e91e8f95c4ebe4dc38cc8a28
caesiumy <mn0658@naver.com>
2019-11-20 21:40:28 +0900

    feat: Change url link

--- 2_restSerializer/post/urls.py
+++ 2_restSerializer/post/urls.py
@@ -18,8 +18,8 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register('post', views.postViewset)
+router.register('posts', views.postViewset)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
 ]
