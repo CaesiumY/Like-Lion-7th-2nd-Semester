e09438475bb9479ee8c2830af57b55c7b4707b4a
CaesiumY <mn0658@naver.com>
2019-11-22 17:13:25 +0900

    chore: Add allowed hosts

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -43,7 +43,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mutsa-portfolio.herokuapp.com']
 
 
 # Application definition
