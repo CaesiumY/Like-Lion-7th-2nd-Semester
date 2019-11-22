4086b5392fafac3b7db072733af95bd9bae9a784
CaesiumY <mn0658@naver.com>
2019-11-22 17:15:32 +0900

    fix: Fix typo

--- main/models.py
+++ main/models.py
@@ -8,7 +8,7 @@ class Portfolio(models.Model):
     body = models.TextField()
     img = models.ImageField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_ap = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
