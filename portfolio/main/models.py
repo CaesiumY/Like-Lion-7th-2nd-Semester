64ecbb68e0856834d24b7e90465037aa192533b8
CaesiumY <mn0658@naver.com>
2019-11-28 11:15:21 +0900

    fix: Fix typo & auto now add

--- portfolio/main/models.py
+++ portfolio/main/models.py
@@ -7,8 +7,8 @@ class Portfolio(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
     img = models.ImageField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_ap = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
