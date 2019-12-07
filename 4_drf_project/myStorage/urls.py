e8db6c25f2fbbeac370cf88a110e4b6d4d2d17f5
CaesiumY <mn0658@naver.com>
2019-12-07 12:37:52 +0900

    feat: Add restapi project features

--- 4_drf_project/myStorage/urls.py
+++ 4_drf_project/myStorage/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import essayViewset
+import rest_framework.urls
 
 router = DefaultRouter()
 router.register('essay', essayViewset)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('api-auth/', include(rest_framework.urls))
 ]
--- 4_drf_project/myStorage/views.py
+++ 4_drf_project/myStorage/views.py
@@ -1,9 +1,27 @@
 from rest_framework import viewsets
 from .serializers import essaySerializer
 from .models import EssayModel
+from rest_framework.filters import SearchFilter
 # Create your views here.
 
 
 class essayViewset(viewsets.ModelViewSet):
     queryset = EssayModel.objects.all()
     serializer_class = essaySerializer
+
+    filter_backends = [SearchFilter]
+    search_fields = ("title", "body")
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        if self.request.user.is_authenticated:
+            qs = qs.filter(author=self.request.user)
+
+        else:
+            qs = qs.none()
+
+        return qs
