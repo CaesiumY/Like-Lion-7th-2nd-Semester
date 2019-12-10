4cc15ba03764aabcffaf29086062bffb9a7e6dc6
CaesiumY <mn0658@naver.com>
2019-12-10 23:40:39 +0900

    feat: Add model

--- 4_drf_project/myStorage/models.py
+++ 4_drf_project/myStorage/models.py
@@ -8,3 +8,18 @@ class EssayModel(models.Model):
         settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     body = models.TextField()
+
+
+class Album(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
+
+    images = models.ImageField(upload_to='images')
+    summary = models.CharField(max_length=100)
+
+
+class File(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
+    files = models.FileField(blank=False, null=False, upload_to='files')
+    summary = models.CharField(max_length=100)
